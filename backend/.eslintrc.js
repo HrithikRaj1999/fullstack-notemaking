ef2827e5e3087c805f9a082592e87fdd59dd2be4
Hrithik Raj <75734812+HrithikRaj1999@users.noreply.github.com>
2023-09-28 23:54:39 +0530

    first commit

---
+++ backend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.export = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    }
+}
